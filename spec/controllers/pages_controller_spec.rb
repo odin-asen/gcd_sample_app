15f21dd0bde144ed8b957584f9d6927147dd3bea
odin-asen <geroge.forman@freenet.de>
2013-02-12 16:43:08 +0000

    Add about page; Add changable title

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+  
+  describe "GET 'home'" do
+    it "should return the right title" do
+      get 'home'
+      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should return the right title" do
+      get 'contact'
+      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should return the right title" do
+      get 'about'
+      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+end
