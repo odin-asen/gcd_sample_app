0636aca81de0ca63d386d4e23cb188e7e85ce7bf
odin-asen <geroge.forman@freenet.de>
2013-02-12 17:01:55 +0000

    Introduce base title for tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,33 +1,34 @@
 require 'spec_helper'
 
 describe PagesController do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   render_views
   
   describe "GET 'home'" do
     it "should return the right title" do
       get 'home'
-      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", content: "#{base_title} | Home")
     end
   end
 
   describe "GET 'contact'" do
     it "should return the right title" do
       get 'contact'
-      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", content: "#{base_title} | Contact")
     end
   end
 
   describe "GET 'about'" do
     it "should return the right title" do
       get 'about'
-      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", content: "#{base_title} | About")
     end
   end
   
   describe "GET 'timetable'" do
     it "should return the right title" do
       get 'timetable'
-      response.should have_selector("title", content: "Ruby on Rails Tutorial Sample App | Timetable")
+      response.should have_selector("title", content: "#{base_title} | Timetable")
     end
   end
 end
