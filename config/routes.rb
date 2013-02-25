4b6d904868dbe74ac09591255e42928e6dfc3a7f
odin-asen <geroge.forman@freenet.de>
2013-02-25 23:22:49 +0000

    Eliminate the need of pages/ in the URL to call a page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/timetable', :to => 'pages#timetable'
+  root :to => 'pages#home'
 
-  get "pages/contact"
-
-  get "pages/about"
-  
-  get "pages/timetable"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
