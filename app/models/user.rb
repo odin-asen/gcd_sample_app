e4f56d6e988d995374bdb7a3bf587d8fbc005792
odin-asen <geroge.forman@freenet.de>
2013-03-26 20:00:08 +0000

    Check user for too long name, valid email address and reject user with duplicate email addresses (case sensitive)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-  validates :name, :presence => true
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence => true, :length => { :maximum => 50}
+  validates :email, :format => { :with => email_regex }, :uniqueness => true
   attr_accessible :email, :name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,22 @@ describe User do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
   end
+  it "should reject names that are too long" do
+    long_name = "a"*51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+  it "should accept a valid email address" do
+    valid_user = User.new(@attr)
+    valid_user.should be_valid
+  end
+  it "should not accept an invalid email address" do
+    invalid_user = User.new(@attr.merge(:email => " "))
+    invalid_user.should_not be_valid
+  end
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_duplicate = User.new(@attr)
+    user_duplicate.should_not be_valid
+  end
 end
