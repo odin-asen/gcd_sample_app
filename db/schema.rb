77b002958377c4933972a6232869dc287c704b63
odin-asen <geroge.forman@freenet.de>
2013-03-29 23:03:39 +0000

    Update database with encrypted password attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130326162758) do
+ActiveRecord::Schema.define(:version => 20130329230026) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "encrypted_password"
   end
 
 end
