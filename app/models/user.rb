9b029ee7eaf4d9e31763298cb21fb1080f32143e
odin-asen <geroge.forman@freenet.de>
2013-03-29 23:13:02 +0000

    Implement user password encryption before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50}
   validates :email, presence: true, :format => { with: email_regex }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, confirmation: true, length: { within: 6..40 }
+  before_save :encrypt_password
+  
+  private
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      string #temporary implementation
+    end
 end
