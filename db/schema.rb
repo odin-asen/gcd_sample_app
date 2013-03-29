4f29d36b646fc2a8d3872c1c3fc6dda796f68f75
odin-asen <geroge.forman@freenet.de>
2013-03-29 23:55:10 +0000

    Add a salt coloumn to the user database. Encrypt the user password with salt.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329230026) do
+ActiveRecord::Schema.define(:version => 20130329235334) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130329230026) do
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
