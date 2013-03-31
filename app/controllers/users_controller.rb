f50d9ec6b538c500ac55435ef1c2fa4a9175ef30
odin-asen <geroge.forman@freenet.de>
2013-03-31 19:13:04 +0100

    Create action for the form button works, but it still accepts empty data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,19 @@ class UsersController < ApplicationController
     @title = "Sign up"
     @user = User.new
   end
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # handle a successful save. need to write tests first
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -35,4 +35,25 @@ describe UsersController do
       response.should have_selector("img", class: "gravatar")
     end
   end
+  
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = {name: "", email: "", password: "", password_confirmation: ""}
+      end
+      it "should not create a user" do
+        lambda do
+          post :create, user: @attr
+        end.should_not change(User, :count)
+      end
+      it "should have the right title" do
+        post :create, user: @attr
+        response.should have_selector("title", content: "Sign up")
+      end
+      it "should render the 'new' page" do
+        post :create, user: @attr
+        response.should render_template('new')
+      end
+    end
+  end
 end
